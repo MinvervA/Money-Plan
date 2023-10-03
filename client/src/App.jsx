a4d6470f269f77ff5849967bd0efad5453e7f5d1
Andrean <andrean923@gmail.com>
2023-10-03 19:30:56 +0700

    test FE

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,13 @@ import "./App.css";
 
 function App() {
   // const [count, setCount] = useState(0)
+  //background-image: linear-gradient(to right bottom, #143f13, #154915, #155317, #155d18, #14681a, #237325, #2f7f30, #3b8a3b, #529951, #68a767, #7db67d, #93c593); Ijo grr
 
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
+    <div className="bg-glass h-screen flex justify-center items-center">
+      <div className="backdrop-blur-md bg-white/30 w-[1300px] h-[700px] rounded-lg"></div>
+      {/* <h1>Hello World!</h1> */}
+    </div>
   );
 }
 
